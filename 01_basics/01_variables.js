a15cdc6dd4ca55531a9379b554cd9c071ff10958
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-05-15 18:45:14 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 100
+let accountEmail = "praj@gmail.com"
+var accountPassword = "1234354"
+accountCity = "Thane"
+
+//accountId = 200 // not aloow //cannot be reassigned
+
+accountEmail = "prajakta@mail.com"
+accountPassword = "12345678"
+accountCity = "Mumbai"
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
