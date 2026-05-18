6b85a04fd1b7345c55721838c519793e6dfc5261
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-05-18 15:51:00 +0000

    config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+//Data Types in JS
+//Number, String, Boolean, Undefined, Null, BigInt, Symbol //These are primitive data types
+
+let age = 21;
+let price = 100.5; 
+console.log(age, price);
+console.log(typeof age); //number
+
+let fullName = "Prajakta Chandrakant Suryarao";
+console.log(fullName);
+console.log(typeof fullName); //String
+
+let isFollow = true;
+console.log(typeof isFollow); //Boolean
+
+let name;
+console.log(name); //undefined
+
+let a = null;
+console.log(a); //null
+
+let x = BigInt("1234");
+let y = Symbol("Hello");
+console.log(typeof x); //bigint
+console.log(typeof y); //symbol
