bad7ca496f8e326d37c154113c95fc929611ea28
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-05-18 14:58:26 +0000

    config done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,11 +3,12 @@ let accountEmail = "praj@gmail.com"
 var accountPassword = "1234354"
 accountCity = "Thane"
 
+
 //accountId = 200 // not aloow //cannot be reassigned
 
 accountEmail = "prajakta@mail.com"
 accountPassword = "12345678"
 accountCity = "Mumbai"
 
-
-console.table([accountId, accountEmail, accountPassword, accountCity])
+console.log(accountId, accountEmail);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
