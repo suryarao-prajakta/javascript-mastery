ac59f5cbb9cdfd199900c543b77574309b5f5673
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-05-19 04:50:06 +0000

    config done

---
+++ 01_basics/03_datatypes.js
@@ -0,0 +1,10 @@
+// Non-Primitive data types : objects(Arrays, Functions)
+
+// Objects: it is a collection of kay values pair
+
+const student = {
+    fullName : "Sakshi Rao",
+    age: 20,
+    cgpa: 8.2,
+    isPass: true,
+};
