3042ba4713cad0465e77085d5d7b0d8721b36973
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-05-21 08:40:02 +0000

    conversion & operation

--- 01_basics/04_conversionOperation.js
+++ 01_basics/04_conversionOperation.js
@@ -11,3 +11,26 @@ let fullName = "Prajakta";
 let isFill = Boolean(fullName);
 console.log(typeof isFill);
 
+//**********Operation*************//
+
+let value = 3 
+let negValue = -value;
+
+console.log(negValue);
+ 
+console.log(2+2); //addition
+console.log(2-2); //substraction
+console.log(2*2); //multiplication
+console.log(2/2); //division
+console.log(2%2); //modulo
+console.log(2**3); //2 res to the power 2
+
+let str1 ="Prajakta";
+let str2 =" Chandrakant";
+let str3 =" Suryarao";
+ 
+let str4 = str1 + str2 + str3;
+// console.log(str1 + str2 + str3);
+console.log(str4);
+
+console.log("1" + 23); 
