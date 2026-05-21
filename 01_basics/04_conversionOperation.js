de8b9cd671afeb644bb20ce84b369b641a4c0407
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-05-21 05:44:36 +0000

    conversion of datatypes

---
+++ 01_basics/04_conversionOperation.js
@@ -0,0 +1,13 @@
+// conversion
+
+let someNumber = 55
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber); 
+
+let fullName = "Prajakta";
+
+let isFill = Boolean(fullName);
+console.log(typeof isFill);
+
