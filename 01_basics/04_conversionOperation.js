1e71cbe93e4fa828259bfcac36dcfb57f89ca83c
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-05-21 16:10:48 +0000

    conversion & operation in js

--- 01_basics/04_conversionOperation.js
+++ 01_basics/04_conversionOperation.js
@@ -1,5 +1,3 @@
-// conversion
-
 let someNumber = 55
 
 let stringNumber = String(someNumber)
@@ -11,20 +9,31 @@ let fullName = "Prajakta";
 let isFill = Boolean(fullName);
 console.log(typeof isFill);
 
-//**********Operation*************//
+// *****Operators in js ******//
+// used to perform some opperation on data
 
-let value = 3 
-let negValue = -value;
+// Arithmetic Operators +,-,*,/
 
-console.log(negValue);
- 
-console.log(2+2); //addition
-console.log(2-2); //substraction
+console.log(2+2); //Addition
+console.log(2-2); //Substraction
 console.log(2*2); //multiplication
 console.log(2/2); //division
-console.log(2%2); //modulo
-console.log(2**3); //2 res to the power 2
+console.log(2%2); //modulus //remaindus
+console.log(2**3); //Exponentiation //2^3
 
+//Unary Operator
+let a = 5;
+let b = 2;
+
+console.log("a=",a,"& b=",b);
+a = a + 1; //a++ //a=5+1//6
+console.log("a=",a); 
+
+//Practice
+let value = 3 
+let negValue = -value;
+console.log(negValue);
+ 
 let str1 ="Prajakta";
 let str2 =" Chandrakant";
 let str3 =" Suryarao";
@@ -32,5 +41,4 @@ let str3 =" Suryarao";
 let str4 = str1 + str2 + str3;
 // console.log(str1 + str2 + str3);
 console.log(str4);
-
 console.log("1" + 23); 
