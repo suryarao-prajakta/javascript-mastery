fb19f0f09fd6617afaa2dc3d9c16539b09386d49
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-05-21 09:08:16 +0000

    comparisons in js

---
+++ 01_basics/05_comparison.js
@@ -0,0 +1,17 @@
+//basics comparisons
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2 < 1);
+console.log(2 == 2);
+console.log(2 != 1);
+
+//avoid this types of comparisons
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null >=0);
+// console.log(null <=0);
+
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
