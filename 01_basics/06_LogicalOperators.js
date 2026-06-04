86fae557821b6fe5ff86ddcb12df02f657e3f456
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-04 12:33:46 +0000

    Logical operators

---
+++ 01_basics/06_LogicalOperators.js
@@ -0,0 +1,33 @@
+// Logical operators in js 
+//3. Logical NOT operator --> !
+
+//1. Logical AND operator -->  && 
+//true true --> true
+//true false --> false
+//fales true --> false
+//fales false --> false
+
+// let a = 10;
+// let b = 15;
+
+// let cond1 = a < b ; //true
+// let cond2 = 10 > 15; //false
+
+// console.log("cond1 && cond2=", cond1 && cond2); //false
+
+//2. Logical OR operator --> ||
+//if any one condition was true output will be true
+
+// let a = 100 ;
+// let b = 59 ; 
+
+// console.log("cond1 || cond2=", a < 59 || 59 >100); //both cond are false so output was // false 
+
+// 3. Logical NOT operator --> !
+// True --> false
+// false --> true
+
+let a = 40;
+let b = 50;
+
+console.log("!(40<50=)", !(a<b)); //true --> output : false
