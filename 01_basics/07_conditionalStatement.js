e81cab5c6b9f8ac18c6425503850fa522a1727a9
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-04 13:02:42 +0000

    conditional Statements

---
+++ 01_basics/07_conditionalStatement.js
@@ -0,0 +1,22 @@
+// let age = 13;
+
+// if (age >= 18) {
+//     console.log("You Can Vote");
+// };
+
+// if (age < 18) {
+//     console.log("You CANNOT Vote");
+// };
+
+let mode = "dark";
+let color;
+
+if (mode === "dark") {
+    color = "black";
+}
+
+if (mood === "light") {
+    color = "white";
+}
+
+console.log(color);
