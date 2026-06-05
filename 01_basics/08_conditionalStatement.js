2591d21b6762a82c5a7faadb53549b7742c2b914
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-05 15:57:36 +0000

    else-if Statement

---
+++ 01_basics/08_conditionalStatement.js
@@ -0,0 +1,16 @@
+// else-if statement
+
+let mode = "pink";
+let color;
+
+if (mode === "dark") {
+    color = "black";
+} else if (mode === "pink") {
+    color = "pink";
+} else if (mode === "blue") {
+    color = "blue";
+} else {
+    color = "white";
+}
+
+console.log(color);
