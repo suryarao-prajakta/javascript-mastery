c94d1b19661a2d110f0d36f20a83c075eb5f6f60
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-05 15:38:20 +0000

    if & if-else statements

--- 01_basics/07_conditionalStatement.js
+++ 01_basics/07_conditionalStatement.js
@@ -1,3 +1,5 @@
+// if statement
+
 // let age = 13;
 
 // if (age >= 18) {
@@ -8,15 +10,44 @@
 //     console.log("You CANNOT Vote");
 // };
 
-let mode = "dark";
-let color;
+// let mode = "dark";
+// let color;
 
-if (mode === "dark") {
-    color = "black";
-}
+// if (mode ==="dark") {
+//     color = "black";
+// }
 
-if (mood === "light") {
-    color = "white";
-}
+// if (mode ==="light") {
+//     color = "white";
+// }
 
-console.log(color);
+// console.log(color);
+
+//if-else statement
+
+// let mode = "light-mode";
+// let color;
+
+// if (mode === "dark-mode") {
+//     color = "black";
+// } else {
+//     color = "white";
+// }
+
+// console.log(color);
+
+// let age = 21;
+
+// if (age >= 18) {
+//     console.log("You can Vote");
+// } else {
+//     console.log("Your CANNOT Vote");
+// }
+
+let num = 20;
+
+// if (num%2===0) {
+//     console.log(num,"is even number");
+// } else {
+//     console.log(num,"is odd number");
+// }
