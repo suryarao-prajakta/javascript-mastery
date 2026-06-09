b4521528c38505d28201bc6d1e5c31374a73eaf5
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-09 04:22:26 +0000

    Ternary Operators

--- 01_basics/09_ternaryOperators.js
+++ 01_basics/09_ternaryOperators.js
@@ -10,3 +10,19 @@ console.log(result);
 // let age = 12;
 
 // console.log(age >= 18 ? "adult" : "not adult");
+
+//practice
+
+// alert("hello"); //one time popup
+
+// let name = prompt("enter name"); 
+// console.log(name);
+
+//Q1 Get user to input a number using prompt("Enter a number:").Check it the number is a multiple of 5 or not 
+// let num = prompt("enter a number:");
+
+// if(num % 5=== 0) {
+//     console.log(num, "is multiple of 5");
+// }else{
+//     console.log(num, "is NOT multiple of 5");
+// }
