8c7e422283be7377d5fa687f6d7ab7253bcb973d
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-09 04:21:50 +0000

    else-if statements

--- 01_basics/08_conditionalStatement.js
+++ 01_basics/08_conditionalStatement.js
@@ -1,16 +1,34 @@
 // else-if statement
 
-let mode = "pink";
-let color;
+// let mode = "pink";
+// let color;
 
-if (mode === "dark") {
-    color = "black";
-} else if (mode === "pink") {
-    color = "pink";
-} else if (mode === "blue") {
-    color = "blue";
-} else {
-    color = "white";
+// if (mode === "dark") {
+//     color = "black";
+// } else if (mode === "pink") {
+//     color = "pink";
+// } else if (mode === "blue") {
+//     color = "blue";
+// } else {
+//     color = "white";
+// }
+
+// console.log(color);
+
+//Q2 write o code which can give grades to students according to their scores: 90-100,A; 70-89,B; 60-69,C; 50-59,D; 0-49,F;
+
+let score=81.84;
+
+if(score >=90 && score <=100 ) {
+    grade = "A"
+}else if (score >= 70 && score <= 89) {
+    grade = "B"
+}else if (score >= 60 && score <= 69) {
+    grade = "C"
+}else if (score >=50 && score <= 59) {
+    grade = "D"
+}else {
+    grade ="F"
 }
 
-console.log(color);
+console.log("according to your scores, your grade was :",grade);
