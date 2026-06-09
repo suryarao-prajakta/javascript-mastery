9efd22dcecc309f632fdf2c88a4ddbed2ddeb5fe
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-09 03:36:44 +0000

    Ternary Operators

---
+++ 01_basics/09_ternaryOperators.js
@@ -0,0 +1,12 @@
+//Ternary Operators : codition? true Output : false output
+
+let age = 22;
+
+let result = age >= 18 ? "adult" : "not adult";
+console.log(result);
+
+
+
+// let age = 12;
+
+// console.log(age >= 18 ? "adult" : "not adult");
