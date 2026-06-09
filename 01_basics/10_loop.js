9b738a27f618886d1fe1ee6006e223ee83f55377
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-09 13:27:58 +0000

    for, infinite, while & do-while loops

---
+++ 01_basics/10_loop.js
@@ -0,0 +1,40 @@
+//Loops in js : Loops are used to execute a piece of code again & again
+
+//for Loop //for(initialization ; stoping condition; updation)
+
+// for(let i = 1; i <=5; i++) {
+//     console.log("i=",i);
+// }
+
+//calculate sum of 1 to 5
+
+// let sum = 0;
+// let n = 10;
+
+// for (let i = 1; i <= n; i++) {
+//     sum = sum + i;
+// }
+
+// console.log("sum=",sum);
+
+// infinite Loop: A loop that never ends 
+
+// for (let i = 1; 1 <=0; 1++) {
+//     console.log("i =", i ); 
+// }
+
+//while Loop
+
+// let i = 1;
+// while(i <= 5) {
+//     console.log("i =",i) ;
+//     i++;
+// }
+
+//do-while Loop
+let i = 1;
+
+do {
+    console.log("i =", i)
+    i++
+}while(i <=10);
