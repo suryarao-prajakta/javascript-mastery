77b6a36c2890e474931e256a73f94da16be9d7e8
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-09 03:36:15 +0000

    if & if-else statements

--- 01_basics/07_conditionalStatement.js
+++ 01_basics/07_conditionalStatement.js
@@ -46,8 +46,8 @@
 
 let num = 20;
 
-// if (num%2===0) {
-//     console.log(num,"is even number");
-// } else {
-//     console.log(num,"is odd number");
-// }
+if (num%2===0) {
+    console.log(num,"is even number");
+} else {
+    console.log(num,"is odd number");
+}
