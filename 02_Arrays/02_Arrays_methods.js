0811c1abbe1a131472c58ef9a434697ad06b6224
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-10 18:51:35 +0000

    Arrays Methods in js

--- 02_Arrays/02_Arrays_methods.js
+++ 02_Arrays/02_Arrays_methods.js
@@ -1,13 +1,41 @@
-//Array Methods 
-//push(): add to end
-let foodItems = ["potato","Apple", "Litchi", "Tomato"]
-foodItems.push("Chips", "burger", "paneer");
+// //Array Methods 
+// //push(): add to end
+// let foodItems = ["potato","Apple", "Litchi", "Tomato"]
+// foodItems.push("Chips", "burger", "paneer");
 
-console.log(foodItems);
+// console.log(foodItems);
 
-//pop(): delete from end & return
-let fruits = ["Apple", "Litchi", "Bannana", "Mango"]
-console.log(fruits);
+// //pop(): delete from end & return
+// let fruits = ["Apple", "Litchi", "Bannana", "Mango"]
+// console.log(fruits);
 
-let deletedItem = fruits.pop();
-console.log(fruits);
+// let deletedItem= fruits.pop();
+// console.log(fruits);
+// console.log("deleted", deletedItem);
+
+// //toString(): converts array to string
+// let num= [1,2,3,4,5,6,7,8,9];
+// console.log(num); //Array 
+// console.log(num.toString()); //String
+
+// //concat(): joins multiple arrays & returns result
+// let marvelheroes = ["thor", "spidermn", "itonman"];
+// let dcheroes = ["superman", "batman"];
+
+// heroes=marvelheroes.concat(dcheroes);
+// console.log(heroes);
+
+// //unshift(): add to start
+// let marvelheroes = ["thor", "spidermn", "itonman"];
+
+// marvelheroes= marvelheroes.unshift("antman");
+
+//shift(): 
+let marvelheroes = ["thor", "spidermn", "itonman"];
+console.log(marvelheroes);
+
+// let val = marvelheroes.shift();
+// console.log(marvelheroes);
+// console.log("deleted", val);
+
+
