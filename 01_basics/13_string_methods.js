6197d9323f11680272a81cd02024183ff578f08d
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-10 05:07:05 +0000

    string methods in js

---
+++ 01_basics/13_string_methods.js
@@ -0,0 +1,22 @@
+//string Methods in js
+//these are built-in function to manipulate a string
+
+//str.toUpperCase()
+
+// let str = "hello world";
+// str = str.toUpperCase(); 
+// console.log(str);
+
+//str.toLowerCase()
+
+// let str = "Hello World";
+// str = str.toLowerCase();
+// console.log(str);
+
+//str.trim() //removes whitespaces in starting and ending
+
+let str = "             Hello world        ";
+console.log(str);
+console.log(str.trim());
+
+//str.slice(start, end?) // returns part of string
