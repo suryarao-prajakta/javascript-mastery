814bc6bff537a95cdaf885c304f57a233a9afbd2
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-10 12:02:21 +0000

    Arrays in js

--- 02_Arrays/01_Arrays.js
+++ 02_Arrays/01_Arrays.js
@@ -1,4 +1,10 @@
 //Arrays in js: Collections of items
 
-let marks = [97, 82, 78, 69,79];
-console.log(marks);
+// let marks = [97, 82, 78, 69,79];
+// console.log(marks);
+// console.log(marks.length);
+
+// let heroes = ["ironman", "Thor", "Hulk", "Shaktiman", "Spiderman", "Antman"];
+// console.log(heroes);
+
+//Array indices
