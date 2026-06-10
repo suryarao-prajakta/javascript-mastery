25bde5b54e9805420dd89512e8f7d802bc9c5992
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-10 18:22:42 +0000

    Arrays Methods in js

---
+++ 02_Arrays/02_Arrays_methods.js
@@ -0,0 +1,13 @@
+//Array Methods 
+//push(): add to end
+let foodItems = ["potato","Apple", "Litchi", "Tomato"]
+foodItems.push("Chips", "burger", "paneer");
+
+console.log(foodItems);
+
+//pop(): delete from end & return
+let fruits = ["Apple", "Litchi", "Bannana", "Mango"]
+console.log(fruits);
+
+let deletedItem = fruits.pop();
+console.log(fruits);
