97b09c250eaa03f3864db3609490487bf2c7d644
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-10 19:16:03 +0000

    Arryas methos in js

---
+++ 02_Arrays/03_Arrays_methods.js
@@ -0,0 +1,27 @@
+//slice(): returns a piece of array
+// let marvelheroes = ["thor", "spidermn", "ironman", "antman", "Dr.Strange"];
+
+// console.log(marvelheroes);
+
+// console.log(marvelheroes.slice(1,3));//output: spiderman,ironman //ending index is non enclusive
+
+//splice(): change original array(add, remove, replace)
+
+// let arr = [1,2,3,4,5,6,7];
+
+// arr.splice(2,2,101,102);
+// console.log(arr);
+
+// let arr = [1,2,3,4,5,6,7];
+// arr.splice(2, 0, 101);
+// console.log(arr);
+
+//delete element
+// let arr = [1,2,3,4,5,6,7];
+// arr.splice(3,1);
+// console.log(arr);
+
+// //Replace element
+// let arr = [1,2,3,4,5,6,7];
+// arr.splice(3, 1,101);
+// console.log(arr);
