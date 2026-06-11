533547e97d9a8d3013a9d896be2d795d04b878e4
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-11 16:33:45 +0000

    functions in js

--- 03_functions&methods/01_functions.js
+++ 03_functions&methods/01_functions.js
@@ -52,3 +52,21 @@ function multiply(x, y) {
 }
 
 multiply(2,9);
+
+//practice 
+//Q1 Create a function using the "function" keyword that takes a String as an argument & returns the number of vowels in the string.
+function countVowels(str) {
+    let count = 0;
+    for(const char of str){
+        if (char=="a" ||
+            char=="e" ||
+            char=="i" ||
+            char=="o" ||
+            char=="u" 
+        ) { 
+           count++;
+        }
+    }
+    console.log(count);
+}
+countVowels("abcdefghij");
