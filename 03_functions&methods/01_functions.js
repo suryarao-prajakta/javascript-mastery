72bc8c7760d65679fc0908fc6cd00c99ce1e1917
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-11 15:39:11 +0000

    functions in js

--- 03_functions&methods/01_functions.js
+++ 03_functions&methods/01_functions.js
@@ -2,10 +2,39 @@
 
 
 //Define function
-function myFunction() {
-    console.log("Prajakta");
-    console.log("I am learning js");
-}
+// function myFunction() {
+//     console.log("hie i am a Prajakta");
+//     console.log("I am learning js");
+// }
 
-//call function
-myFunction();
+// //call(invoke) function
+// myFunction();
+// myFunction();
+
+
+// function newFunction(msg) {
+//     //Parameter-> input
+//     console.log(msg);
+// }
+
+// newFunction("I am learning JavaScript"); //argumet
+// newFunction(); //output: undefined
+
+
+// //function-> sum of 2 numbers
+
+// function sum(x, y) {
+//     console.log(x + y);
+// }
+
+// sum(3,3);
+
+// function sum(x, y ){
+//     //function parameters:are like local variables -> block scope of function
+//     s= x + y;
+//     return s;
+// }
+
+// let val = sum(4,5);
+// console.log(val);
+
