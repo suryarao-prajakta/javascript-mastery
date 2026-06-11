8a61ac3943abce136b8a1c52e2bd3c7051086434
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-06-11 10:33:27 +0000

    funtions

---
+++ 03_functions&methods/01_functions.js
@@ -0,0 +1,11 @@
+// functions: Blocks of code that performs a specific task, can be invoked needed
+
+
+//Define function
+function myFunction() {
+    console.log("Prajakta");
+    console.log("I am learning js");
+}
+
+//call function
+myFunction();
