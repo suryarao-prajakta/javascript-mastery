370034499831e47d03bbe3f682ebfc613849a7bd
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-07-13 19:49:55 +0530

    added practice quetion

---
+++ 10_Practice-Questions/Q1_Print-Hello-World/script.js
@@ -0,0 +1,10 @@
+// 1.Hello World Program using console.log() function
+console.log("Hello World");  // This line prints "Hello World" to the console
+
+
+// 2.Hello World Program using alert() function 
+alert("Hello World"); // This line shows an alert box with the message "Hello World"
+
+
+// 3.Hello World Program using document.write() function 
+document.write("Hello World"); // This line writes "Hello World" directly to the HTML document/HTML page
---
+++ 10_Practice-Questions/Q2_Add-Two_numbers/script.js
@@ -0,0 +1,18 @@
+// 1.Program to add static numbers using console.log() function
+let a = 5;
+let b =43;
+
+var sum = a + b;
+
+console.log("The sum of " + a +"and" + b + " is: " + sum); //+ do addition and concatenate the string
+
+
+
+// 2.Program to add dynamic numbers using prompt() function 
+let x = prompt("Enter first number:");
+let y = prompt("Enter second number:");
+
+var sum = parseInt(x) + parseInt(y);
+
+console.log("The sum of " + x + "and" + y + "is:" + sum);
+ 
