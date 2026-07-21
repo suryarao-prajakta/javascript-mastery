3fdb8a27220fa8de82877c43a4ad652b55be0efc
suryarao-prajakta <prajaktasuryarao75@gmail.com>
2026-07-21 16:09:55 +0530

    Question 6

---
+++ Q7_convert-celsius-to-fahrenheit/script.js
@@ -0,0 +1,10 @@
+// convert celsius to fahrenheit
+
+function convert() {
+    let celsius = document.getElementById("data").value;
+    let fahrenheit = (celsius * 1.8) + 32;
+    document.getElementById("get").innerText = `${celsius} celsius = ${fahrenheit} fahrenheit`;
+    console.log(`${celsius} celsius = ${fahrenheit} fahrenheit`)
+}
+
+
